b14a2bd4ab425619ca07fac2f1447dd49dbcf00f
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-08-29 18:41:23 -0300

    exercício feito

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fraseDigitada,  fraseMemetoNervoso; 
+            Console.Write("Usuário, o que você pensa sobre mim?" );
+            fraseDigitada = Console.ReadLine();
+
+              fraseMemetoNervoso = 
+              $"{fraseDigitada.Remove(10)} ... NÃO, PERA.\n Tô nervoso";
+
+              Console.WriteLine(fraseMemetoNervoso);
+              
+
+
+      
+
+
+        }
+    }
+}
